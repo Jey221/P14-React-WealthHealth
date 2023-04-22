9a9c5d8aacacabf9873b2b4a8f5306634263b356
Jérémy Koundour <j.koundour@gmail.com>
2023-04-22 21:47:43 +0200

    mise en place de MUI pour la mise en forme de l'app

--- src/pages/list.js
+++ src/pages/list.js
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom';
 import TableauList from '../components/List/tableau';
+import { Typography } from '@mui/material';
 
 function List(employees) {
   return (
     <div>
-      <h1>Current Employees</h1>
+      <Typography variant="h2">hello</Typography>
       <TableauList employees={employees} />
       <Link to={'/'}>Home</Link>
     </div>
