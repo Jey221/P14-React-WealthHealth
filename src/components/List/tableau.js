d3d03db57be75c10f5de5a9f8dbac1632bc75858
Jérémy Koundour <j.koundour@gmail.com>
2023-04-22 19:41:54 +0200

    mise en place du tableau avec react-table

--- src/components/List/tableau.js
+++ src/components/List/tableau.js
@@ -1,78 +1,74 @@
-import { useState } from 'react';
+import React, { useMemo } from 'react';
+import MaterialReactTable from 'material-react-table';
 
-function TableauList(employees) {
-  // mise en place hook pour la saisie ds la barre de recherche
-  const [searchField, setSearchField] = useState('');
-  // création de la liste des employées filtrés
-  const filteredEmployees = employees.employees.employees.filter((employee) => {
-    return (
-      employee.firstName
-        .toLowerCase()
-        .includes(searchField.toLocaleLowerCase()) ||
-      employee.lastName
-        .toLowerCase()
-        .includes(searchField.toLocaleLowerCase()) ||
-      employee.departement
-        .toLowerCase()
-        .includes(searchField.toLocaleLowerCase()) ||
-      employee.city.toLowerCase().includes(searchField.toLocaleLowerCase()) ||
-      employee.state.toLowerCase().includes(searchField.toLocaleLowerCase()) ||
-      employee.street.toLowerCase().includes(searchField.toLocaleLowerCase()) ||
-      employee.startDate.includes(searchField.toLocaleLowerCase()) ||
-      employee.birthday.includes(searchField.toLocaleLowerCase()) ||
-      employee.zipCode.includes(searchField.toLocaleLowerCase())
-    );
-  });
-  //écouteur sur la barre de saisie
-  const searchChange = (e) => {
-    setSearchField(e.target.value);
-  };
+//nested data is ok, see accessorKeys in ColumnDef below
+const Example = (employee) => {
+  const employeeList = employee.employees.employees;
+  //should be memoized or stable
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: 'firstName', //access nested data with dot notation
+        header: 'First Name',
+      },
+      {
+        accessorKey: 'lastName',
+        header: 'Last Name',
+      },
+      {
+        accessorKey: 'startDate', //normal accessorKey
+        header: 'Start Date',
+      },
+      {
+        accessorKey: 'departement',
+        header: 'Department',
+      },
+      {
+        accessorKey: 'birthday',
+        header: 'Date of Birth',
+      },
+      {
+        accessorKey: 'street',
+        header: 'Street',
+      },
+      {
+        accessorKey: 'city',
+        header: 'City',
+      },
+      {
+        accessorKey: 'state',
+        header: 'State',
+      },
+      {
+        accessorKey: 'zipCode',
+        header: 'Zip Code',
+      },
+    ],
+    []
+  );
 
   return (
-    <div>
-      <div>
-        <p>Search:</p>
-      </div>
-      <div>
-        <input className="searchBar" type="search" onChange={searchChange} />
-      </div>
-      <table className="employeesList">
-        <thead>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Start Date</th>
-            <th>Departement</th>
-            <th>Date of Birth</th>
-            <th>Street</th>
-            <th>City</th>
-            <th>State</th>
-            <th>zip Code</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredEmployees.map((employee) => (
-            <tr className="employeeList" key={employee.lastName}>
-              <td>{employee.firstName}</td>
-              <td>{employee.lastName}</td>
-              <td>{employee.startDate}</td>
-              <td>{employee.departement}</td>
-              <td>{employee.birthday}</td>
-              <td>{employee.street}</td>
-              <td>{employee.city}</td>
-              <td>{employee.state}</td>
-              <td>{employee.zipCode}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <div className="indexEmployees">
-        <p>
-          Showing 1 to {filteredEmployees.length} of {filteredEmployees.length}
-        </p>
-      </div>
-    </div>
+    <MaterialReactTable
+      columns={columns}
+      data={employeeList}
+      initialState={{
+        showGlobalFilter: true,
+      }}
+      enableToolbarInternalActions={false}
+      enableColumnActions={false}
+      defaultColumn={{
+        size: 10,
+        minSize: 5,
+        maxSize: 20,
+      }}
+      muiTablePaperProps={{
+        sx: {
+          maxWidth: '1050px',
+          m: 'auto',
+        },
+      }}
+    />
   );
-}
+};
 
-export default TableauList;
+export default Example;
